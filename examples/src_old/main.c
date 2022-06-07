1ef3962a65888005d470fcc21ddb31c22989b2b0
FeedehC <feedehc@gmail.com>
2022-06-07 11:18:53 -0300

    chore: repository cleanup

---
+++ examples/src_old/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "welcome.h"
+
+int main()
+{
+    printf("Hola Mundo!\n");
+    printf("%s", str_welcome());
+    return 0;
+}
--- src/main.c
+++ src/main.c
@@ -1,11 +1,25 @@
-#include <stdio.h>
-#include <string.h>
+#include <driver/gpio.h>
+// Include FreeRTOS for delay
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 
-#include "welcome.h"
+#define LED 2 // LED connected to GPIO2
 
-int main()
-{
-    printf("Hola Mundo!\n");
-    printf("%s", str_welcome());
-    return 0;
+int app_main() {
+    // Configure pin
+    gpio_config_t io_conf;
+    io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    io_conf.pin_bit_mask = (1ULL << LED);
+    io_conf.pull_down_en = 0;
+    io_conf.pull_up_en = 0;
+    gpio_config(&io_conf);
+
+    // Main loop
+    while(true) {
+        gpio_set_level(LED, 0);
+        vTaskDelay(500 / portTICK_RATE_MS);
+        gpio_set_level(LED, 1);
+        vTaskDelay(500 / portTICK_RATE_MS);
+    }
 }
